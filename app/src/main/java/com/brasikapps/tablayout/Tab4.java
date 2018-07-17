d272d42ac9b69205dcae07a82628cbd974cddc7a
Ibrahim Kipkemoi <Ibrahim@Ibrahims-MacBook-Air.local>
2018-07-17 13:46:31 +0300

    Initial commit

---
+++ app/src/main/java/com/brasikapps/tablayout/Tab4.java
@@ -0,0 +1,23 @@
+package com.brasikapps.tablayout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ibrahim on 12/07/2018.
+ */
+
+public class Tab4 extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab4,container,false);
+    }
+}
